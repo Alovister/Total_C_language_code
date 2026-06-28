4229d8fd0575385684cfdcbc28e1cb457dcb594a
Alovister <3039558684@qq.com>
2026-06-28 23:32:04 +0800

    学习 C 语言和 Python 的文本编辑文件

---
+++ C_language.c
@@ -0,0 +1,15 @@
+// 打开终端后输入“ gcc C_language.c -o C_language ”生成.exe文件，再输入“ ./C_language.exe ”运行
+
+// p77
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    
+
+    printf("\n \n");
+    system("pause");
+    return EXIT_SUCCESS;
+}
